a2c7c29e54d23ce70baa4f790ba8dac0bf5797dc
Chris Bongers <chrisbongers@gmail.com>
2020-10-09 07:55:08 +0200

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SecondComponent } from './second/second.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ChildComponent } from './child/child.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent }
+  { path: 'welcome', component: WelcomeComponent, children: [
+    { path: 'about', component: AboutComponent }
+  ] },
+  { path: 'second', children: [
+    { path: '', component: SecondComponent, },
+    { path: 'child', component: ChildComponent }
+  ] },
+  { path: '',   redirectTo: '/welcome', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { SecondComponent } from './second/second.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ChildComponent } from './child/child.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    SecondComponent,
+    NotFoundComponent,
+    ChildComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
